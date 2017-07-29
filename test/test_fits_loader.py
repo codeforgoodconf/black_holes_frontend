2cdf86644802ed2ae66434b3fc5e35c3c8403a28
Nathaniel Saul <nat@saulgill.comt>
2017-07-29 11:03:11 -0700

    tests for fits loader

--- test/test_fits_loader.py
+++ test/test_fits_loader.py
@@ -5,6 +5,12 @@ from app.source.FitsLoader import FitsLoader
 
 
 def test_loaded():
-    assert 0
+    fl = FitsLoader()
+    fits = fl.load_fits("spectrum_data/spec-1342-52793-0537.fits")
+
+    assert isinstance(fits, tuple)
+    assert len(fits) == 2
+    assert isinstance(fits[0], list)
+
 
 
